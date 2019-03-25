462299f5e13618ec71377bc9184e5dcb940e105f
gerardohernandez20 <42582681+gerardohernandez20@users.noreply.github.com>
2019-03-25 10:14:59 -0700

    hew

---
+++ p5/p5-exercise-1/sketch.js
@@ -0,0 +1,19 @@
+
+// declare global variables here
+
+
+function setup() {
+  // put setup code here --> this runs once upon launch
+  createCanvas(600, 500);
+
+}
+
+
+function draw() {
+  // this loops over and over
+
+}
+
+
+
+// write custom functions here
